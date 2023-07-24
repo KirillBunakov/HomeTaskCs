06783611f9121902dcc1f8e7327ad4cedb6a878a
KirillBunakov <lonley987@gmail.com>
2023-07-24 13:31:39 +0300

    test commit

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 1
+// Console.Write("Введите число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// int result = a*a;
+// Console.WriteLine(result);
+
+// Задача 2
+// Console.Write("Введите число 1: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите число 2: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+//   if (num1 == num2*num2)
+//   {
+//     Console.WriteLine("True");
+//   }
+//   else
+//   {
+//     Console.WriteLine("False");
+//   }
+
+// Задача 3
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// if (num == 1) {Console.WriteLine("Понедельник");}
+// else if (num == 2) {Console.WriteLine("Вторник");}
+// else if (num == 3) {Console.WriteLine("Среда");}
+// else if (num == 4) {Console.WriteLine("Четверг");}
+// else if (num == 5) {Console.WriteLine("Пятница");}
+// else if (num == 6) {Console.WriteLine("Суббота");}
+// else if (num == 7) {Console.WriteLine("Воскресенье");}
+// else {Console.WriteLine("Такого дня недели нет");}
+
+// Задача 4
+// Console.Write("Введите число: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// int numNeg = num1*-1;
+
+// while (numNeg<=num1)
+// {
+//   Console.WriteLine(numNeg);
+//   numNeg++;
+// }
+
+// Задача 5
+Console.Write("Введите число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int result = num1%10;
+Console.WriteLine(result);
+test
+
+
+
+
